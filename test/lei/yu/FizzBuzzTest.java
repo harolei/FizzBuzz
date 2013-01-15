6439dca7b498f0fd87aeec649394e01a2cb63ba2
harolei <harolei@gmail.com>
2013-01-15 15:40:09 +0800

    #2 [harolei] Refactored the FizzBuzz

--- test/lei/yu/FizzBuzzTest.java
+++ test/lei/yu/FizzBuzzTest.java
@@ -1,20 +1,27 @@
 package lei.yu;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void should_output_fizz_when_input_is_divisible_by_3() throws Exception {
-       FizzBuzz fizzBuzz = new FizzBuzz();
-       assertThat(fizzBuzz.run(3),is("Fizz"));
+        assertThat(fizzBuzz.run(3),is("Fizz"));
     }
 
     @Test
     public void should_output_buzz_when_input_is_divisible_by_5() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertThat(fizzBuzz.run(5),is("Buzz"));
     }
 }
