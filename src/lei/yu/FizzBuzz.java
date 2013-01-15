6d533690b389a9ab42b98be845c26de2373eca05
harolei <harolei@gmail.com>
2013-01-15 14:54:27 +0800

    #1 [harolei] Wrote the test for the Fizz, and faked it.

---
+++ src/lei/yu/FizzBuzz.java
@@ -0,0 +1,14 @@
+package lei.yu;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: leiyu
+ * Date: 13-1-15
+ * Time: 下午12:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class FizzBuzz {
+    public String run(int inputNum) {
+        return "Fizz";  //To change body of created methods use File | Settings | File Templates.
+    }
+}
