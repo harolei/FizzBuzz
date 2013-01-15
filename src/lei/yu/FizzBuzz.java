285d56623ff731af9d6f922d2defd93fd152562b
harolei <harolei@gmail.com>
2013-01-15 15:28:29 +0800

    #2 [harolei] Completed the test and implement of Buzz.

--- src/lei/yu/FizzBuzz.java
+++ src/lei/yu/FizzBuzz.java
@@ -1,16 +1,11 @@
 package lei.yu;
 
-/**
- * Created with IntelliJ IDEA.
- * User: leiyu
- * Date: 13-1-15
- * Time: 下午12:43
- * To change this template use File | Settings | File Templates.
- */
 public class FizzBuzz {
     public String run(int inputNum) {
         if (inputNum % 3 == 0) {
             return "Fizz";
+        }else if(inputNum % 5 == 0){
+            return "Buzz";
         }
         return "";
     }
--- test/lei/yu/FizzBuzzTest.java
+++ test/lei/yu/FizzBuzzTest.java
@@ -5,17 +5,16 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-/**
- * Created with IntelliJ IDEA.
- * User: leiyu
- * Date: 13-1-15
- * Time: 下午12:37
- * To change this template use File | Settings | File Templates.
- */
 public class FizzBuzzTest {
     @Test
     public void should_output_fizz_when_input_is_divisible_by_3() throws Exception {
        FizzBuzz fizzBuzz = new FizzBuzz();
        assertThat(fizzBuzz.run(3),is("Fizz"));
+    }
+
+    @Test
+    public void should_output_buzz_when_input_is_divisible_by_5() throws Exception {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.run(5),is("Buzz"));
     }
 }
